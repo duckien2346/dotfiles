b798750a26713ad5c87f95f38aded185c4176f84
kiensh <91678288+kiensh@users.noreply.github.com>
2026-07-20 20:00:01 +0700

    backup: 2026-07-20 20:00:00   - scripts/alias.sh

--- scripts/alias.sh
+++ scripts/alias.sh
@@ -32,3 +32,4 @@ fi
 [[ $(command -v cursor) ]] && alias cur=cursor
 [[ $(command -v claude) ]] && alias cla=claude
 [[ $(command -v opencode) ]] && alias op=opencode
+[[ $(command -v aria2c) ]] && alias a=aria2c
